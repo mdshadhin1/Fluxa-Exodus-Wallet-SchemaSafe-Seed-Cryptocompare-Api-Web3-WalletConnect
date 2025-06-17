035b59c9aa7d765eded2c8950925571d79d2687d
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-17 21:15:01 +0300

    analogio.d

---
+++ Fluxa-Exodus-Wallet/exodus/core/analogio.d
@@ -0,0 +1,21 @@
+src/core/analogio.d: ../src/core/analogio.cpp ../src/core/analogio.h \
+ /Users/lynxeyed/Documents/LPCXpresso_5.1.0/workspace/eXodusino/src/core/device_dependent/lpc1114/gpio.h \
+ /Users/lynxeyed/Documents/LPCXpresso_5.1.0/workspace/eXodusino/src/core/device_dependent/lpc1114/lpcdevice.h \
+ /Users/lynxeyed/Documents/LPCXpresso_5.1.0/workspace/eXodusino/src/core/device_dependent/lpc1114/LPC11xx.h \
+ /Users/lynxeyed/Documents/LPCXpresso_5.1.0/workspace/eXodusino/src/core/device_dependent/lpc1114/core_cm0.h \
+ /Users/lynxeyed/Documents/LPCXpresso_5.1.0/workspace/eXodusino/src/core/device_dependent/lpc1114/system_LPC11xx.h \
+ /Users/lynxeyed/Documents/LPCXpresso_5.1.0/workspace/eXodusino/src/core/device_dependent/lpc1114/system_LPC11xx.h
+
+../src/core/analogio.h:
+
+/Users/lynxeyed/Documents/LPCXpresso_5.1.0/workspace/eXodusino/src/core/device_dependent/lpc1114/gpio.h:
+
+/Users/lynxeyed/Documents/LPCXpresso_5.1.0/workspace/eXodusino/src/core/device_dependent/lpc1114/lpcdevice.h:
+
+/Users/lynxeyed/Documents/LPCXpresso_5.1.0/workspace/eXodusino/src/core/device_dependent/lpc1114/LPC11xx.h:
+
+/Users/lynxeyed/Documents/LPCXpresso_5.1.0/workspace/eXodusino/src/core/device_dependent/lpc1114/core_cm0.h:
+
+/Users/lynxeyed/Documents/LPCXpresso_5.1.0/workspace/eXodusino/src/core/device_dependent/lpc1114/system_LPC11xx.h:
+
+/Users/lynxeyed/Documents/LPCXpresso_5.1.0/workspace/eXodusino/src/core/device_dependent/lpc1114/system_LPC11xx.h:
